af297ce82265fc45ed2c78aaa1eb1a8244ee6c42
JBF <john@codeintheschools.org>
2021-07-01 12:13:01 -0400

    Update ScoreTracker.cs
    Cleaned out some old code

--- Assets/Scripts/ScoreTracker.cs
+++ Assets/Scripts/ScoreTracker.cs
@@ -17,7 +17,6 @@ public class ScoreTracker : MonoBehaviour
     {
         if (OnScore == null) OnScore = new UnityEvent<int>();
         OnScore.AddListener(HandleScore);
-		//GameState.OnEnd.AddListener(ResetScore);
 
     }
 
@@ -25,51 +24,12 @@ public class ScoreTracker : MonoBehaviour
     {
         currentScore += num;
 		scoreText.text = currentScore.ToString();
-        //Debug.LogWarning("New Score: " + currentScore.ToString());
         
     }
 
     public void ResetScore()
 	{
-        //Debug.LogWarning("Resetting");
 		currentScore = 0;
 		scoreText.text = currentScore.ToString();
 	}
-
-
-    #region Old BowlStuff
-    static List<int> scores = new List<int>();
-    public static void ReportScore(int newScore)
-    {
-        scores.Add(newScore);
-    }
-
-    public int GetFinalScore()
-    {
-        int finalScore = 0;
-        for(int i = 0; i < scores.Count; i++)
-        {
-            finalScore += scores[i];
-        }
-        return finalScore;
-    }
-
-    public int[] GetScores()
-    {
-        int[] scoreArray = new int[scores.Count];
-        scores.CopyTo(scoreArray);
-
-        return scoreArray;
-    }
-
-    public int GetScore(int which)
-    {
-        int score = 0;
-        if(scores.Count > which)
-        {
-            score = scores[which];
-        }
-        return score;
-    }
-    #endregion
 }
